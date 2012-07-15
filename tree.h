cdb2234a7d27fad4c8efaed294c8ddbaaa4c1c63
Domrachev <miked@bk.ru>
2012-07-15 23:00:34 +0700

    изменения в дереве

--- tree.h
+++ tree.h
@@ -51,37 +51,35 @@
 		NSLog(@ "zashel %i",2);
 		[root AddNum: _Num: root->left];
 		}
-		NSLog(@ "proebalsya1");
+		
 	if(root->right->Num==nil&&root->Op!=nil)
 		{NSLog(@ "zashel %i",3);
 		[root AddNum: _Num: root->right];
 		}
-		NSLog(@ "proebalsya2");
+		
 }
 -(void)AddOp:(char)iOp
 {	if(Op==nil)
-	{	//NSLog(@ "proebalsya1");
+	{	
 		Op=iOp;
 		return;
 	}
-	//NSLog(@ "proebalsya2");
+	
 	Node *tmp1=self;
 	Node *tmp2=[[Node alloc]init];
 	[tmp2 InsertOp: iOp];
 	
 	if(iOp=='-'||iOp=='+')
-	{	NSLog(@ "proebalsya - +");
+	{	
 		tmp2->left=tmp1;
 		self=tmp2;
-		//left=tmp1;
-		//NSLog(@ "%c",self->Op);
-		//NSLog(@ "%c",self->left->Op);
+		
 	}
 	else
-	{	//NSLog(@ "proebalsya inache");
+	{	
 		right=tmp2;
 	}
-	//NSLog(@ "%c",Op);
+	
 	return;
 }
 
